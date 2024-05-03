5efbab0f5f9f8239d657c175ed7d4311137a3924
tid213 <tid2136141@gmail.com>
2024-05-02 17:54:53 -0700

    changed server.js to build react app

--- server.js
+++ server.js
@@ -159,9 +159,16 @@ app.get('/notebook/:id', async (req, res) => {
 
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on port 8000.`);
