f171874a51f8577ae659b7e11fab92aff5da3abb
tid213 <tid2136141@gmail.com>
2024-06-26 16:06:53 -0700

    changed fetch priority on home page landing image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,6 +12,7 @@ const usePreloadImage = (imageUrl) => {
     useEffect(() => {
       const link = document.createElement('link');
       link.rel = 'preload';
+      link.fetchPriority = "high";
       link.as = 'image';
       link.href = imageUrl;
       document.head.appendChild(link);
