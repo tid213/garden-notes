ee3052d5795dcf544750d2da884ea027c658fd1e
tid213 <tid2136141@gmail.com>
2024-07-11 16:23:53 -0700

    resized landing image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -104,16 +104,14 @@ function Home () {
                 </div>
             )}
             </header>
-            <section className=" min-w-screen  flex flex-col mt-16">
-                <div className="lg:flex lg:items-center flex flex-col items-center w-full min-h-screen">
+            <section className=" min-w-screen  flex flex-col lg:mt-4">
+                <div className="lg:flex lg:items-center flex flex-col justify-center items-center w-full min-h-screen">
                 <img
                     className="mb-4 max-w-[384px]  lazyload"
-                    src={isMobile ? landingImageSmall : landingImage}
-                    srcSet={`${landingImageSmall} 384w, ${landingImage} 512w`}
-                    sizes="(max-width: 767px) 384px, 512px"
+                    src={landingImageSmall}
                     alt="Two people gardening"
-                    height={isMobile ? 384 : 512}
-                    width={isMobile ? 384 : 512}
+                    height={264}
+                    width={264}
                 />
                 <div className="w-full lg:w-6/12 mx-auto text-center">
                     <p className="font-normal mb-4 px-8 text-2xl text-customDarkGreen">Welcome to</p>
